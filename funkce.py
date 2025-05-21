db2b9291898b72f9c9c0c0be15c2339814848e66
unknown <vilma.pohsoltova@infis.cz>
2025-05-21 13:06:37 +0200

    1

---
+++ funkce.py
@@ -0,0 +1,15 @@
+from main import soucet, rozdil
+
+print(soucet(5,1))
+
+
+
+
+def main():
+    num1 = int(input("zadej prvni cislo"))
+    num2 = int(input("zadej prvni cislo"))
+    num3 = int(input("zadej prvni cislo"))
+    operace = input
+    if operace == "+":
+        
+
---
+++ main.py
@@ -0,0 +1,14 @@
+def soucet(a:int,b:int):
+    return(a+b)
+
+print(soucet(5,10))
+
+def rozdil(a:int,b:int):
+    return(a-b)
+
+def vypis(a:str, b: int):
+    a = "ahoj"
+    b = 3
+    for i in range(b):
+        print(a)
+    return vypis
